50211678c768449f085814d6a93801ebd31da4b8
Zac Workman <zac.workman45@gmail.com>
2017-04-05 22:36:48 +0100

    Added DELETE /todos/:id tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -96,3 +96,38 @@ describe('GET /todos/:id', () => {
       .end(done);
   });
 });
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    const hexID = todos[1]._id.toHexString();
+    request(app)
+      .delete(`/todos/${hexID}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexID);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        Todo.findById(hexID).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch(e => done(e));
+      });
+  });
+
+  it('should return a 404 if todo not found', (done) => {
+    request(app)
+      .delete(`/todos/${new ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if objectID is invalid', (done) => {
+    request(app)
+      .delete(`/todos/${"0141SchmellYerMaw"}`)
+      .expect(404)
+      .end(done);
+  });
+});
