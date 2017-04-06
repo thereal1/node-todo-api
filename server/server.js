bf653f1ab5290e9299f2fecf83313198e246e2c7
Zac Workman <zac.workman45@gmail.com>
2017-04-06 20:39:54 +0100

    Setup seperate test and development envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const config = require('./config/config');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const _ = require('lodash');
@@ -10,7 +12,7 @@ const { User } = require('./models/user');
 
 let app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
