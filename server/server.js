7e1a86a22a3d522843e6ae04bb6e00faa2558f0c
Zac Workman <zac.workman45@gmail.com>
2017-04-09 15:51:15 +0100

    Added POST /users/login route

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const { ObjectID } = require('mongodb');
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
-const { authenticate } = require('./middleware/authenticate');  
+const { authenticate } = require('./middleware/authenticate');
 
 let app = express();
 
@@ -17,6 +17,8 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
+
+// POST
 app.post('/todos', (req, res) => {
   let todo = new Todo({
     text: req.body.text
@@ -38,6 +40,17 @@ app.post('/users', (req, res) => {
   }).catch((e) => res.status(400).send(e));
 });
 
+app.post('/users/login', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+  User.findByCredentials(body.email, body.password).then((user) => {
+    user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch(e => res.status(400).send());
+});
+
+
+// GET
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
@@ -61,6 +74,12 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
+
+
+// DELETE
 app.delete('/todos/:id', (req, res) => {
   const id = req.params.id;
   if (!ObjectID.isValid(id)) {
@@ -74,6 +93,8 @@ app.delete('/todos/:id', (req, res) => {
   }).catch(e => res.status(400).send());
 });
 
+
+// PATCH
 app.patch('/todos/:id', (req, res) => {
   const id = req.params.id;
   let body = _.pick(req.body, ['text', 'completed']);
@@ -96,9 +117,6 @@ app.patch('/todos/:id', (req, res) => {
   }).catch((e) => res.status(400).send());
 });
 
-app.get('/users/me', authenticate, (req, res) => {
-  res.send(req.user);
-});
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
